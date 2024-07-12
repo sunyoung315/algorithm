b48cefd855030151931c1c17e8a57799b050844e
sunyoung315 <139304927+sunyoung315@users.noreply.github.com>
2024-07-12 18:46:29 +0900

    [level 1] Title: 모의고사, Time: 4.32 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,48 +1,30 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
+    static int[] oneArr = {1, 2, 3, 4, 5};
+    static int[] twoArr = {2, 1, 2, 3, 2, 4, 2, 5};
+    static int[] treeArr = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
     public int[] solution(int[] answers) {
-        int[] answer = {};
-        
-        int[] score = new int[3];
+
+        Map<Integer, Integer> map = new HashMap<>();
         
         for(int i = 0; i < answers.length; i++) {
-            if((i % 5 == 0 && answers[i] == 1) || (i % 5 == 1 && answers[i] == 2)
-              || (i % 5 == 2 && answers[i] == 3) || (i % 5 == 3 && answers[i] == 4)
-              || (i % 5 == 4 && answers[i] == 5)) 
-                score[0]++;
-            if((i % 8 == 0 && answers[i] == 2) || (i % 8 == 1 && answers[i] == 1)
-                   || (i % 8 == 2 && answers[i] == 2) || (i % 8 == 3 && answers[i] == 3)
-                   || (i % 8 == 4 && answers[i] == 2) || (i % 8 == 5 && answers[i] == 4)
-                   || (i % 8 == 6 && answers[i] == 2) || (i % 8 == 7 && answers[i] == 5))
-                score[1]++;
-            if((i % 10 == 0 && answers[i] == 3) || (i % 10 == 1 && answers[i] == 3)
-                   || (i % 10 == 2 && answers[i] == 1) || (i % 10 == 3 && answers[i] == 1)
-                   || (i % 10 == 4 && answers[i] == 2) || (i % 10 == 5 && answers[i] == 2)
-                   || (i % 10 == 6 && answers[i] == 4) || (i % 10 == 7 && answers[i] == 4)
-                   || (i % 10 == 8 && answers[i] == 5) || (i % 10 == 9 && answers[i] == 5))
-                score[2]++;
+            if(answers[i] == oneArr[i % oneArr.length])
+                map.put(1, map.getOrDefault(1, 0) + 1);
+            if(answers[i] == twoArr[i % twoArr.length])
+                map.put(2, map.getOrDefault(2, 0) + 1);
+            if(answers[i] == treeArr[i % treeArr.length])
+                map.put(3, map.getOrDefault(3, 0) + 1);
         }
         
-        int max = 0;
-        int sameScore = 0;
+        int maxScore = Collections.max(map.values());
         
-        for(int i = 0; i < 3; i++) {
-            if(score[i] > max) {
-                max = score[i];
-                sameScore = 1;
-            } else if(score[i] == max) {
-                sameScore++;
-            }
-        }
+        List<Integer> arr = new ArrayList<>();
+        for(Map.Entry<Integer, Integer> entry: map.entrySet())
+            if(entry.getValue() == maxScore)
+                arr.add(entry.getKey());
         
-        int idx = 0;
-        answer = new int[sameScore];
-        for(int i = 0; i < 3; i++) {
-            if(score[i] == max)
-                answer[idx++] = i + 1;                
-        }
-        
-        return answer;
+        return arr.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 }
