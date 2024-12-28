27c80dcab95627a85c75edc1beaf8b1e9fd77239
sunyoung315 <139304927+sunyoung315@users.noreply.github.com>
2024-12-28 16:58:14 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,29 +1,24 @@
 -- 코드를 작성해주세요
-SELECT 
-    (CASE 
-        WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End') != 0 
-            AND SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python') 
-                = (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python') 
-            THEN 'A'
-        WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#') 
-            = (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#') 
-            THEN 'B'
-        WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End') != 0 
-            THEN 'C'
-    END) AS GRADE,
-    ID,
-    EMAIL
-FROM DEVELOPERS
-WHERE 
-    (CASE 
-        WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End') != 0 
-            AND SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python') 
-                = (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python') 
-            THEN 'A'
-        WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#') 
-            = (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#') 
-            THEN 'B'
-        WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End') != 0 
-            THEN 'C'
-    END) IS NOT NULL
-ORDER BY GRADE ASC, ID ASC;
+WITH grade_table AS (
+  SELECT (CASE 
+            WHEN skill_code & (SELECT code
+                                 FROM skillcodes
+                                WHERE name = 'Python') <> 0
+             AND skill_code & (SELECT SUM(code)
+                                FROM skillcodes
+                               WHERE category = 'Front End') <> 0 THEN 'A'
+            WHEN skill_code & (SELECT code
+                                 FROM skillcodes
+                                WHERE name = 'C#') <> 0 THEN 'B'
+            WHEN skill_code & (SELECT SUM(code)
+                                 FROM skillcodes
+                                WHERE category = 'Front End') <> 0 THEN 'C'
+         END) AS grade,
+         id,
+         email
+    FROM developers
+)
+SELECT *
+  FROM grade_table
+ WHERE grade IS NOT NULL
+ ORDER BY grade, id;
