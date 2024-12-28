8d313733df6dfeb10feca68f953f658bb88f953e
sunyoung315 <139304927+sunyoung315@users.noreply.github.com>
2024-12-29 01:28:15 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,22 +1,20 @@
 -- 코드를 작성해주세요
-WITH RECURSIVE TREE AS (
-    SELECT ID, PARENT_ID, 1 AS GENERATION
-    FROM ECOLI_DATA
-    WHERE PARENT_ID IS NULL
-    
-    UNION ALL
-    
-    SELECT e.ID, e.PARENT_ID, t.GENERATION + 1 AS GENERATION
-    FROM ECOLI_DATA e
-    INNER JOIN TREE t
-    ON e.PARENT_ID = t.ID
+WITH RECURSIVE gen_data AS (
+  SELECT id, parent_id, 1 AS generation
+    FROM ecoli_data
+   WHERE parent_id IS NULL
+   
+   UNION ALL
+  
+  SELECT e.id, e.parent_id, generation + 1 AS generation
+    FROM ecoli_data e
+   INNER JOIN gen_data g
+      ON e.parent_id = g.id
 )
-
-# SELECT * FROM TREE;
-
-SELECT COUNT(ID) AS COUNT, GENERATION
-FROM TREE
-WHERE ID NOT IN (SELECT PARENT_ID FROM TREE t WHERE TREE.GENERATION + 1 = T.GENERATION)
-GROUP BY GENERATION
-ORDER BY GENERATION;
-
+SELECT COUNT(id) AS count, generation
+  FROM gen_data
+ WHERE id NOT IN (SELECT DISTINCT parent_id 
+                    FROM ecoli_data 
+                   WHERE parent_id IS NOT NULL)
+ GROUP BY generation
+ ORDER BY generation;
