5db373989c78fea20b9b63ae14cbafe0b1af6210
sunyoung315 <139304927+sunyoung315@users.noreply.github.com>
2025-01-24 16:54:58 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -3,12 +3,12 @@ class Solution {
         int answer = 0;
         
         while(a != b) {
-            a = (a + 1) / 2;
-            b = (b + 1) / 2;
+            a = a % 2 == 0 ? a / 2 : a / 2 + 1;
+            b = b % 2 == 0 ? b / 2 : b / 2 + 1;
             
             answer++;
-        }        
-        
+        }
+
         return answer;
     }
 }
